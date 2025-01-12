0f9b6890de17571562e92e0b39d85e74a25dd02c
Sagar Rajput <122637057+sagarrajput1810@users.noreply.github.com>
2025-01-12 13:17:17 +0530

    auth controller and company controller was completed

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"
+import companyRoute from "./routes/company.route.js";
+// import { connect } from "mongoose";
+import connectDB from "./utils/db.js";
+dotenv.config({})
+
+const app = express();
+
+
+// Tempary backend server
+// app.get("/home",(req,res)=>{
+//     return res.status(200).json({
+//         message:"I am comming from backend",
+//         success: true
+//     })
+// })
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: 'http/localhost:5173',
+    credentiala:true
+}
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/user",userRoutes);
+app.use("/api/v1/company",companyRoute);
+app.listen(PORT,()=>{
+    connectDB();
+    console.log("Server running at port",PORT);
+})
